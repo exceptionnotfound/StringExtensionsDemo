bb56d74dd108511d83a4256b67602911fa9358d5
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2020-11-05 11:29:28 -0700

    Add project files.

---
+++ StringExtensionsDemo/Extensions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringExtensionsDemo
+{
+    public static class Extensions
+    {
+        public static string RemoveLastOccuranceOf(this string input, string toMatch)
+        {
+            if (input.Contains(toMatch))
+            {
+                int lastIndex = input.LastIndexOf(toMatch);
+                return input.Substring(0, lastIndex);
+            }
+            else return input;
+        }
+
+        public static string RemoveLastOccuranceOf(this string input, char toMatch)
+        {
+            if (input.Contains(toMatch))
+            {
+                int lastIndex = input.LastIndexOf(toMatch);
+                return input.Substring(0, lastIndex);
+            }
+            else return input;
+        }
+
+        public static string ToDelimitedList(this List<string> items, char delimiter = ',')
+        {
+            if (!items.Any()) return string.Empty;
+
+            string result = "";
+
+            foreach(var item in items)
+            {
+                result += item + delimiter;
+            }
+
+            return result.RemoveLastOccuranceOf(delimiter);
+        }
+
+        public static List<string> ToCollection(this string input, char delimiter = ',')
+        {
+            if(!input.Contains(delimiter))
+            {
+                return new List<string> { input };
+            }
+            return input.Split(delimiter).ToList();
+        }
+
+        public static string RemoveCharacters(this string input, List<char> chars)
+        {
+            string result = input;
+            foreach(var c in chars)
+            {
+                result = result.Replace(c.ToString(), string.Empty);
+            }
+
+            return result;
+        }
+
+        public static string Excerpt(this string input, int characters)
+        {
+            if (input.Length <= characters)
+                return input;
+            else return input.Substring(0, characters) + "...";
+        }
+    }
+}
---
+++ StringExtensionsDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringExtensionsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------RemoveLastOccurranceOf---------------");
+            string firstTest = "This is a test string, we will remove everything after the comma.";
+            string secondTest = "This string has no comma so trying to remove it will do nothing.";
+            //We want to remove the comma
+            Console.WriteLine(firstTest.RemoveLastOccuranceOf(','));
+            Console.WriteLine(secondTest.RemoveLastOccuranceOf(','));
+
+            Console.WriteLine("---------------------ToDelimtedList--------------------");
+            List<string> names = new List<string> { "Adam", "Bertie", "Carlissa", "Donovan", "Evie", "Franchesca" };
+            var result = names.ToDelimitedList();
+            Console.WriteLine(result);
+
+            List<string> emptyList = new List<string>();
+            var emptyResult = emptyList.ToDelimitedList('|');
+            Console.WriteLine(emptyResult);
+
+            Console.WriteLine("----------------------ToCollection---------------------");
+            var collection = result.ToCollection();
+            Console.WriteLine(collection);
+
+            Console.WriteLine("--------------------RemoveCharacters-------------------");
+            string test2 = "This; is, a badly! formatted :sentence.";
+            Console.WriteLine(test2.RemoveCharacters(new List<char> { ';', ':', ',', '!' }));
+
+            Console.WriteLine("------------------------Excerpt------------------------");
+            string longText = "This is some long text. Here is a story I can tell about how I lived another day.";
+            Console.WriteLine(longText.Excerpt(20));
+            Console.WriteLine(longText.Excerpt(200));
+            Console.WriteLine(longText.Excerpt(80)); //This one will cut the period off an add the ellipsis (...)
+        }
+    }
+}
